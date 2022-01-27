d507461d3d3b431e2110ba445f4154a692956da6
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2022-01-27 16:08:31 -0500

    compiled safelly in edge device

--- compile_edge.sh
+++ compile_edge.sh
@@ -2,22 +2,27 @@
 
 # cd workspace directory and source ROS dependencies
 cd $HOME/uwo_ws
+mv src/uwo_lab/mesh_open3d .
+mv src/uwo_lab/SC-PGO .
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
+
 # Go through all the packages, sleep in between
-catkin_make -DCMAKE_BUILD_TYPE=Release --pkg livox_ros_driver
+catkin_make -DCMAKE_BUILD_TYPE=Release -j2 --pkg livox_ros_driver
 sleep 1s
 
-catkin_make -DCMAKE_BUILD_TYPE=Release --pkg zed_ros
+catkin_make -DCMAKE_BUILD_TYPE=Release -j2 --pkg zed_ros
 sleep 1s
 
-catkin_make -DCMAKE_BUILD_TYPE=Release --pkg zed_wrapper zed_interfaces zed_nodelets
+catkin_make -DCMAKE_BUILD_TYPE=Release -j2 --pkg zed_wrapper zed_interfaces zed_nodelets
 sleep 1s
 
-catkin_make -DCMAKE_BUILD_TYPE=Release --pkg fast_lio
+catkin_make -DCMAKE_BUILD_TYPE=Release -j1 --pkg fast_lio
 sleep 1s
 
-catkin_make -DCMAKE_BUILD_TYPE=Release --pkg fuse_color_3d
+catkin_make -DCMAKE_BUILD_TYPE=Release -j2 --pkg fuse_color_3d
 sleep 1s
 
+mv mesh_open3d src/uwo_lab/
+mv SC-PGO src/uwo_lab/
